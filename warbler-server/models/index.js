1d0e72b035b8551b933f7a4c36aed6a7304ec496
Mrschabs <49056471+Mrschabs@users.noreply.github.com>
2019-10-23 08:16:35 +0200

    Backend part1 of Warbler app

---
+++ warbler-server/models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/warbler", {
+	keepAlive: true
+	// useMongoClient: true
+});
+
+module.exports.User = require("./user");
+module.exports.Message = require("./message");
