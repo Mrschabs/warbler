22f41d5c435afd8ddad113cd43528002d36e7dc2
Mrschabs <49056471+Mrschabs@users.noreply.github.com>
2019-10-30 11:58:10 +0200

    added ensureCorrectUser function

--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -24,4 +24,20 @@ exports.loginRequired = function(req, res, next){
 };
 
 //make sure we get the correct user - Authorisation
-exports.loginRequired = function(req, res, next){};
+exports.ensureCorrectUser = function(req, res, next){
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded){
+      if(decoded && decoded.id === req.params.id){
+        return next();
+      } else {
+          return next({
+            status: 401,
+            message: "Unauthorized"
+          });
+      }
+    });
+  } catch (e) {
+    return next({ status: 401, message: "Unauthorized" });
+  }
+};
