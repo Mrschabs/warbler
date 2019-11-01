aedc454e5343c70f39201267e4a5b766015796a4
De Wet Blomerus <dewet@blomerus.org>
2019-11-01 10:24:59 +0200

    prettier changes

--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -2,39 +2,39 @@ require("dotenv").load();
 const jwt = require("jsonwebtoken");
 
 //make sure the user is logged in - Authentication
-exports.loginRequired = function(req, res, next){
+exports.loginRequired = function(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded){
-      if(decoded){
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+      if (decoded) {
         return next();
       } else {
         return next({
           status: 401,
-          message: "Please log in first"
+          message: "Please log in first",
         });
       }
     });
   } catch (e) {
     return next({
       status: 401,
-      message: "Please log in first"
+      message: "Please log in first",
     });
   }
 };
 
 //make sure we get the correct user - Authorisation
-exports.ensureCorrectUser = function(req, res, next){
+exports.ensureCorrectUser = function(req, res, next) {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded){
-      if(decoded && decoded.id === req.params.id){
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+      if (decoded && decoded.id === req.params.id) {
         return next();
       } else {
-          return next({
-            status: 401,
-            message: "Unauthorized"
-          });
+        return next({
+          status: 401,
+          message: "Unauthorized",
+        });
       }
     });
   } catch (e) {
