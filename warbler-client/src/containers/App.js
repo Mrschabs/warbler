71dfd6599ee178bc857baa180c8a9ca4b56b5657
Mrschabs <49056471+Mrschabs@users.noreply.github.com>
2019-11-11 09:46:53 +0200

    Added Navbar

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Provider} from "react-redux";
 import {configureStore} from "../store";
 import {BrowserRouter as Router} from "react-router-dom";
+import Navbar from "./Navbar";
 
 const store = configureStore()
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>Hello World!</div>
+      <div className="onboarding">
+        <Navbar />
+      </div>
     </Router>
   </Provider>
 );
---
+++ warbler-client/src/containers/Navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+class Navbar extends Component {
+  render(){
+    return(
+      <nav className="navbar navbar-expand">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            <img src="" alt="Warbler Home" />
+          </Link>
+        </div>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <Link to="/signup">Sign up</Link>
+            </li>
+            <li>
+              <Link to="/signin">Log in</Link>
+            </li>
+          </ul>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    currentUser: state.currentUser
+  };
+}
+
+export default connect(mapStateToProps, null)(Navbar);
